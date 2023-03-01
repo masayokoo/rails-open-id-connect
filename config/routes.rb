32edab16d0782c056f3cc87f44c5fbd272b24524
masayokoo <masahiro.yokoo@medicalnote.co.jp>
2023-03-01 20:57:33 +0900

    Add doorkeeper, open id connect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  use_doorkeeper_openid_connect
+  use_doorkeeper
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
